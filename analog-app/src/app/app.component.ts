2cf60f9730cbcd5c34c88d20ae66aa4036f49727
Esther White <monacodelisa@gmail.com>
2024-03-28 11:27:58 +0200

    restored to the prev deployed stage

--- analog-app/src/app/app.component.ts
+++ analog-app/src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./components/footer.component";
+import { HeaderComponent } from "./components/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
-  styles: [
-    `
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-    `,
-  ],
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, RouterOutlet, HeaderComponent, FooterComponent],
+	template: `
+		<app-header />
+		<router-outlet />
+		<app-footer />
+	`,
+	styles: [
+		`
+			:host {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				min-height: 100vh;
+				width: 100vw;
+				overflow-x: hidden;
+			}
+		`,
+	],
 })
 export class AppComponent {}
