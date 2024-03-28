6085a84d1873980cc68b9af71bd46528790dbaf6
Esther White <monacodelisa@gmail.com>
2024-03-28 14:03:03 +0200

    apollo added same as angular -standalone getting eror

--- analog-app/src/app/app.config.ts
+++ analog-app/src/app/app.config.ts
@@ -1,12 +1,32 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideFileRouter } from '@analogjs/router';
+import { ApplicationConfig } from "@angular/core";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideFileRouter } from "@analogjs/router";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+
+import { APOLLO_OPTIONS, Apollo } from "apollo-angular";
+import { HttpLink } from "apollo-angular/http";
+import { ApolloClientOptions, InMemoryCache } from "@apollo/client/core";
+
+const uri = "https://gql.hashnode.com/"; // <-- add the URL of the GraphQL server here
+
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+	return {
+		link: httpLink.create({ uri }),
+		cache: new InMemoryCache(),
+	};
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideFileRouter(),
-    provideHttpClient(withFetch()),
-    provideClientHydration(),
-  ],
+	providers: [
+		provideFileRouter(),
+		provideClientHydration(),
+		provideHttpClient(),
+		provideHttpClient(withFetch()),
+        {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+    Apollo
+	],
 };
